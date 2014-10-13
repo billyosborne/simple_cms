fb4f556f4e893949b6012037807cca8ee0b21054
Billy Osborne <billy.osborne@gmail.com>
2014-10-13 12:05:31 -0400

    Initialize repository

---
+++ db/migrate/20140927221300_create_pages.rb
@@ -0,0 +1,20 @@
+class CreatePages < ActiveRecord::Migration
+  def up
+    create_table :pages do |t|
+    	t.string "name"
+    	t.string "permalink"
+    	t.integer "position"
+    	t.boolean "visible", :default => false
+    	t.integer "subject_id"
+		t.timestamps
+	end
+	add_index("pages", "subject_id")
+	add_index("pages", "permalink")
+  end
+
+  def down
+  	remove_index("pages", "permalink")
+  	remove_index("pages", "subject_id")
+  	drop_table :pages
+  end
+end
